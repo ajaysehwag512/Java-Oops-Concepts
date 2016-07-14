9f9bd6933ee7da724bcaf01ce80ce121bee9cedd
Ajay Kumar <ajaysehwag512@gmail.com>
2016-07-14 14:27:38 -0400

    Java OOPS Concepts Sample Programs

---
+++ src/InstanceofOperatorExample.java
@@ -0,0 +1,7 @@
+public class InstanceofOperatorExample {
+	public static void main(String[] args){
+		String Name = "ajay";
+		boolean result = (Name) instanceof String;
+		System.out.println(result);
+	}
+}
